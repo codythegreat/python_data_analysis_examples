9bb0e1b9a1a0e2378b4e228b852da7b361668d3d
codythegreat <maxiecody@gmail.com>
2019-10-08 18:57:47 -0500

    cleaned up code and added regex for score

--- footballAnalytics.py
+++ footballAnalytics.py
@@ -1,38 +1,43 @@
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
+import pprint
+import re
 
-wb = load_workbook(filename = 'book1.xlsx') # todo : add in worksheet names
-workSheets = []
+wb = load_workbook(filename = 'Football-2019.xlsx') # todo : add in worksheet names
+workSheets = wb.sheetnames[:5]
 teams = []
 # fill teams list with lists of [team1, team2, winner]
 for ws in workSheets:
     for row in range(2, 16):
-        gameData = []
-        gameData.append(wb.ws['A'+str(row)].value.split(' at '))
-        gameData.append(wb.ws['M'+str(row)].value)
+        gameData = wb[ws]['A'+str(row)].value.split(' at ')
+        gameData.append(wb[ws]['M'+str(row)].value)
         teams.append(gameData)
 
 players = []
 # fill players list from spreadsheet
-for col in range(2, 12):
-    players.append(wb.active['A'+str(col)].value)
+for col in range(2, 13):
+    players.append(wb.active.cell(column=col, row=1).value)
 # create a new dictionary holding each players game scores by week
 winsByWeekByPlayer = dict() 
 for ws in workSheets:
     colCounter = 2
-    for week in range(1, 8):
-        # create a new key to week with an empty string value
-        winsByWeekByPlayer[week] = []
-        for player in players:
+    # create a new key to week with an empty string value
+    winsByWeekByPlayer[ws] = []
+    for player in players:
             # new list to hold each pick score
-            scoreByGame = []
-            for row in range(2, 16):
-                scoreByGame.append(wb.ws.cell(row=row, column=colCounter).value)
-                # test to see if this works: determine win by fill color
-                # if it doesn't try start_color
-                if wb.ws.cell(row=row, column=colCounter).fill.fill_type == none:
-                    scoreByGame.append(0)
-                else:
-                    scoreByGame.append(1)
-            colCounter = colCounter + 1
-            winsByWeekByPlayer[week].append({player: scoreByGame})
+        scoreByGame = []
+        for row in range(2, 19):
+            # since some weeks are shorter, break when reach score
+            if re.search(r"^\s*\d+-\d+\s*$", wb[ws].cell(row=row, column=colCounter).value):
+                break
+            # determine win by fill color
+            if wb[ws].cell(row=row, column=colCounter).fill.fill_type == 'solid':
+                scoreByGame.append(1)
+            elif wb[ws].cell(row=row, column=colCounter).fill.fill_type == None:
+                scoreByGame.append(0)
+        colCounter = colCounter + 1
+        winsByWeekByPlayer[ws].append({player[:3].upper(): scoreByGame})
+
+pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(winsByWeekByPlayer[1])
+print(winsByWeekByPlayer['Week 4'][-1])
