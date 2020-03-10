abd6604679228ba4b237befad4edea9b23b82740
codythegreat <maxiecody@gmail.com>
2020-03-10 19:39:29 +0000

    seperate female from male balances

--- json_example/jsonTest.py
+++ json_example/jsonTest.py
@@ -5,22 +5,27 @@ import plotly.graph_objects as go
 data = json.load(open('MOCK_DATA.json'))
 
 # assign money values to list as float data type
-numberData = []
+maleBalances = []
+femaleBalances = []
 for person in data:
+    print(person.items())
     for key, value in person.items():
-        if key == "balance":
+        if key == "gender" and value == "F":
             # convert to float (excluding dollar sign)
-            numberData.append(float(value[1:]))
+            femaleBalances.append(float(person.get("balance")[1:]))
+        elif key == "gender" and value == "M":
+            # convert to float (excluding dollar sign)
+            maleBalances.append(float(person.get("balance")[1:]))
 
-x = np.arange(1, 101)
+x = np.arange(1, len(femaleBalances))
 print(x[1])
 
 fig = go.Figure(go.Scatter(
     x = x,
-    y = numberData
+    y = femaleBalances
 ))
 
 fig.update_layout(
-    title = 'Random number data plotted to a scatter line'
+    title = 'Female Balances'
 )
 fig.show()
