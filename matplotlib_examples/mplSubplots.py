57e07f3a9996b48b80ced05447213a358562ea0b
codythegreat <maxiecody@gmail.com>
2020-03-11 06:41:00 -0500

    added spacing around similar code

--- matplotlib_examples/mplSubplots.py
+++ matplotlib_examples/mplSubplots.py
@@ -3,12 +3,14 @@ import numpy as np
 
 # adjust size of figure to better fit screen
 plt.figure(figsize=(8,7))
+
 # create a 2x1 subplot using a green line to plot the data points
 plt.subplot(2, 1, 1)
 x1 = [1,2,3,4,5]
 y1 = [239, 431, 454, 555, 334]
 plt.plot(x1, y1, "g-")
-plt.title("Joey Hunger Level") #my cat
+plt.title("Joey Hunger Level") # my cat
+
 # create a 2x1 subplot using a red line to plot the data points
 plt.subplot(2,1,2)
 x2 = [1,2,3,4,5]
