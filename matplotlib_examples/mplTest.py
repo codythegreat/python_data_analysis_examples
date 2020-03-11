9be9e0fa73d616220b94d6b926796691722b009a
codythegreat <maxiecody@gmail.com>
2020-03-11 06:58:06 -0500

    added spacing, comments, figure titles

--- matplotlib_examples/mplTest.py
+++ matplotlib_examples/mplTest.py
@@ -4,12 +4,22 @@ import numpy as np
 # create an array from 1-5
 x = np.arange(1, 6)
 y = x**3
+
 # use 10x5 as the figure size
 plt.figure(figsize=(10,5))
+
 # plot two sets of data, one with blue dots and another with red triangles
 plt.plot([1,2,3,4,5], [1,4,3,9,21], "bo", x, y, "r^")
-# set titles and show the figure
-plt.title("Title of Plot")
-plt.xlabel("X Label")
-plt.ylabel("Y Label")
+
+# set title
+plt.title("Sales and Employee Count by Quarter")
+
+# set the legend
+plt.legend(["Employee Count", "Sales"])
+
+# set the XY labels
+plt.xlabel("Quarter")
+plt.ylabel("Dollars (per thousand)")
+
+# finally, show the figures
 plt.show()
