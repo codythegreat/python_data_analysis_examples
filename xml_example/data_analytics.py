89e59a6799b2f629a3ceec1c359185620e01b155
codythegreat <maxiecody@gmail.com>
2020-03-15 08:45:32 -0500

    format code more consistently

--- xml_example/data_analytics.py
+++ xml_example/data_analytics.py
@@ -2,33 +2,30 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
 
+while __name__ == "__main__":
+    dataPointsById = {} 
+    years = []
+    def parseXML(xmlfile):
+        # grab the XML file and bring it into python
+        tree = ET.parse(xmlfile)
+        root = tree.getroot()
+        # add the data to our dictionary
+        for child in root:
+            dataPointsById[child[0].text] = [child[4].text, child[5].text]
 
-dataPointsById = {} 
-years = []
-def parseXML(xmlfile):
-    # grab the XML file and bring it into python
-    tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    # add the data to our dictionary
-    for child in root:
-        dataPointsById[child[0].text] = [child[4].text, child[5].text]
+    parseXML("dataset.xml")
 
-parseXML("dataset.xml")
+    # create a 10x8 plot
+    plt.figure(figsize=(10,8))
 
-#for key, value in dataPointsById.items():
-#    print(str(key).ljust(4) + " : " + value[0].ljust(6)+ " - " + value[1])
-
-# create a 10x8 plot
-plt.figure(figsize=(10,8))
-
-# create an empty list and store data in it
-dataToPlot = []
-for key, value in dataPointsById.items():
-    dataToPlot.append(value[1])
-# create a numpy int array from our list
-dataToPlot = np.array(dataToPlot, dtype='int')
-# create our x and y values from the count of times each year occurs
-x, y = np.unique(dataToPlot, return_counts=True)
-# create and show the scatter plot
-plt.scatter(x, y)
-plt.show()
+    # create an empty list and store data in it
+    dataToPlot = []
+    for key, value in dataPointsById.items():
+        dataToPlot.append(value[1])
+    # create a numpy int array from our list
+    dataToPlot = np.array(dataToPlot, dtype='int')
+    # create our x and y values from the count of times each year occurs
+    x, y = np.unique(dataToPlot, return_counts=True)
+    # create and show the scatter plot
+    plt.scatter(x, y)
+    plt.show()
