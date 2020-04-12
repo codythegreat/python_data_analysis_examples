472d2b9acc9a63ccf2156b82ca474734239d8936
codythegreat <maxiecody@gmail.com>
2020-04-12 16:49:13 -0500

    simplify conversion to dataframe and add filtering example

--- csv_examples/csvToDataframe.py
+++ csv_examples/csvToDataframe.py
@@ -2,33 +2,22 @@
 #                  creates a pandas DataFrame from data
 #                  and prints cars in order of horsepower
 
-import csv
-import numpy as np
 import pandas as pd
 
-csvFile = open("MOCK_DATA.csv")
-readCSV = csv.reader(csvFile, delimiter=",")
+# One of the easiest ways to manipulate and print CSV data
+# in Python is to use Pandas to parse the file into a 
+# DataFrame object
+df = pd.read_csv("MOCK_DATA.csv")
+print(df)
 
-# use a pandas DataFrame to store the data
-# we don't need the id when creating the dataframe
-# so we'll just use 5 lists
-rawData = [[],[],[],[],[]]
-for row in readCSV:
-    rawData[0].append(row[1]) 
-    rawData[1].append(row[2]) 
-    rawData[2].append(row[3]) 
-    rawData[3].append(row[4]) 
-    rawData[4].append(row[5]) 
+print("")
 
-carDataDict = {
-    rawData[0][0]: rawData[0][1:],
-    rawData[1][0]: rawData[1][1:],
-    rawData[2][0]: rawData[2][1:],
-    rawData[3][0]: rawData[3][1:],
-    rawData[4][0]: rawData[4][1:]
-}
+# using the read_csv function will create a DataFrame from
+# the data in the CSV file. From there we can filter it
+print(df.loc[df['make'].isin(['Toyota', 'Suzuki', 'Honda'])])
 
-carDF = pd.DataFrame(carDataDict)
-# print the entire dataframe by horsepower ascending
-# .to_string() is used to print entire table
-print(carDF.sort_values("horsepower").to_string()) 
+print("")
+
+# We can also print out the data based on each row's values
+# for instance, we can print out the cars based on horsepower
+print(df.sort_values(by='horsepower', ascending=False))
